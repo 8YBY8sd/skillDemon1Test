76b2351b43ee30c020b0af01c9d488ea7c356bab
8YBY8sd <b9yu@ucsd.edu>
2022-05-01 14:28:46 -0700

    update

--- skillDemonOneTestTester.java
+++ skillDemonOneTestTester.java
@@ -2,7 +2,7 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
-public class skillDemon1TestTester {
+public class skillDemonOneTestTester {
     @Test
     public void addition() {
         assertEquals(1, skillDemon1Test.add(1,1));
